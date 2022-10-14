a9a6776f731234d5ca68d09dd93c8ee3334ebda4
Diego Guilherme <diegoguilherme752@oultook.com>
2022-10-14 15:35:40 -0300

    Entrega GitSearch

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,156 @@
 /* Desenvolva sua lógica aqui...*/
+
+
+function interationForm() {
+    let inputUserName = document.getElementById("input-login")
+    let buttonSearchUser = document.getElementById("button-search")
+
+    inputUserName.addEventListener("keydown", () => {
+        buttonSearchUser.disabled = false
+    })
+
+
+    buttonSearchUser.addEventListener("click", (event) => {
+        event.preventDefault()
+        let buttonSearch = event.path[0]
+        let valueInput = inputUserName
+        /* takeUserAPI(valueInput, buttonSearch) */
+        searchUserGitHub(valueInput, buttonSearch)
+    })
+}
+
+function searchUserGitHub(userInput, button){
+    button.disabled = true
+    button.innerHTML = `
+        <img class="icon-search" src="/assets/img/spinner.png" alt="icone de procura">
+    `
+    
+    const response = fetch(`https://api.github.com/users/${userInput.value}`)
+    .then(async resp => {
+        
+        if (!resp.ok) {
+            let inputUserName = document.getElementById("input-login")
+
+            inputUserName.insertAdjacentHTML("afterend", `<h3 class="user-underfind">usuario não encontrado</h3>`)
+            inputUserName.classList.toggle("input-fail")
+
+            setTimeout(() => {
+                let userUnderFind = document.querySelector(".user-underfind")
+                userUnderFind.remove()
+                inputUserName.classList.toggle("input-fail")
+            }, 1055)
+
+            
+        } else {
+            let data = await resp.json()
+        
+            let userData = {
+                img: data.avatar_url,
+                user: `${userInput.value}`,
+                userName: data.name
+            }
+
+            addListRecentUser(userData)
+            SetUserProfileCurrent(userData.user)
+            location.replace("/pages/profile/index.html");
+        }
+    })
+    .catch(err => console.log(err))
+    
+
+    setTimeout(() => {
+        button.innerHTML = "Ver perfil do github"
+        userInput.value = ''
+    }, 1055)
+}
+
+interationForm()
+
+
+
+/* recent list */
+
+function takeListRecentLocalStorage() {
+    return JSON.parse(localStorage.getItem("@ListRecentUser")) || []
+}
+
+const listRecent = Array.from(takeListRecentLocalStorage())
+
+function addListRecentUser(user) {
+    let list = listRecent
+
+    if (list.length <= 2) {
+        
+        if (list.length === 0) {
+            localStorage.setItem("@ListRecentUser", JSON.stringify([user]))
+        } else {
+            list.forEach(element => {
+                const findUser = list.find((userCurrent) => {
+                    return userCurrent.userName === user.userName
+                })
+
+                if (findUser === undefined) {
+                    localStorage.setItem("@ListRecentUser", JSON.stringify([...list, user]))
+                }
+            })
+        }
+    } else {
+        list.forEach(element => {
+            const findUser = list.find((userCurrent) => {
+                return userCurrent.userName === user.userName
+            })
+
+            if (findUser === undefined) {
+                localStorage.setItem("@ListRecentUser", JSON.stringify([...list, user]))
+                list.splice(0, 1)
+            }
+        })
+    }   
+}
+
+
+function renderRecentUser(list) {
+    let listRecent = document.querySelector(".list-recent")
+    if (list.length > 0) {
+        
+        let span = document.createElement("span")
+        span.classList.add("text-2")
+        span.innerText = "Achados Recentemente:"
+
+        listRecent.append(span)
+        
+        list.forEach(user => {
+            let profileRecent = document.createElement("li")
+            profileRecent.classList.add("profile-recent")
+            profileRecent.id = user.userName
+
+            profileRecent.innerHTML = `
+            <a href="#" onclick="showProfileRecent(event)">
+                <img class="img-recent" src="${user.img}" alt="foto de perfil">
+            </a>
+            <span class="text-5">Acessar este perfil</span>
+            `
+
+            listRecent.appendChild(profileRecent)
+        })
+    }
+}
+
+renderRecentUser(listRecent)
+
+
+function showProfileRecent(event) {
+    let pathId = event.path[2].id
+    const findUser = listRecent.find(user => user.userName === pathId)
+
+    SetUserProfileCurrent(findUser.user)
+    
+    setTimeout(() => {
+        location.replace("/pages/profile/index.html");
+    }, 1055)
+}
+
+
+function SetUserProfileCurrent(user) {
+    return localStorage.setItem("@currentUser", JSON.stringify(user))
+}
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,73 @@
 /* Desenvolva sua lógica aqui...*/
+const baseUrl = "https://api.github.com/users"
+
+async function renderInforUser(user) {
+    const response = await fetch(`${baseUrl}/${user}`)
+    .then(async resp => {
+
+        if (!resp.ok) {
+            console.log(resp.status)
+        }
+
+        let data = await resp.json()
+        let {avatar_url, bio, name} = data
+        
+        let containerHeader = document.querySelector(".container-header")
+
+        containerHeader.insertAdjacentHTML("afterbegin", `
+            <div class="container-infor-user">
+                <img src="${avatar_url}" alt="foto de perfil">
+                <div class="infor-user">
+                    <h1 class="title-2">${name}</h1>
+                    <span class="text-2">${bio}</span>
+                </div>
+            </div>
+        `)
+    })
+    .catch(err => console.log(err))
+
+    renderRepositoriesUser(user)
+}
+
+
+async function renderRepositoriesUser(user) {
+    const response = await fetch(`${baseUrl}/${user}/repos`)
+    .then(async resp => {
+        
+        let data = await resp.json()
+        data.map(respo => {
+            const {name, description, html_url} = respo
+            
+            let listRepositories = document.querySelector(".list-repositories")
+
+            let repository = document.createElement("li")
+            repository.classList.add("repository")
+
+            repository.innerHTML = `
+                <h2 class="title-3">${name}</h2>
+                <p class="text-4">${description}</p>
+                <div class="buttons-repositories">
+                    <a class="button-second-rosa" href="${html_url}" target="_blank">Repositório</a>
+                    <button class="button-second-rosa">Demo</button>
+                </div>
+            `
+
+            listRepositories.appendChild(repository)
+        })
+
+        
+    }).catch(err => console.log(err))
+}
+
+
+function changeUser() {
+    location.replace("/pages/home/index.html");
+}
+
+
+function takeUserProfile() {
+    return JSON.parse(localStorage.getItem("@currentUser"))
+}
+
+
+renderInforUser(takeUserProfile())
